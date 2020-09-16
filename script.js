8267eb7d5fcd8e77b73c19289d45d6718761f3d2
Wesley-Snoei <111585@emmauscollege.nl>
2020-09-16 09:12:48 +0000

    kleine shit

--- script.js
+++ script.js
@@ -13,42 +13,27 @@ function setup() {
 
 X = 640;
 Y = 360;
-SpeedX = 1;
-SpeedY = 1;
+SpeedX = 30;
+SpeedY = 30;
 
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
+
 function draw() {
 
-  background('blue');
-  // stel vulkleur in
+  background(0, 0, 255));
   fill(100, 100, 255);
   ellipse(X,Y,80,80);
-  // teken een cirkel
 
     X = X + SpeedX;
     Y = Y + SpeedY;
 
-    if(X <= 40) {
-        SpeedX = 1;
-        SpeedY = 1;
+    if(X <= 40 || X >= 1040) {
+        SpeedX = SpeedX * -1;
+        SpeedY = SpeedY * -1;
     }
 
-    if(X >= 1040) {
-        SpeedX = -1;
-        SpeedY = -1;
+    if(Y <= 40) || Y >= 680) {
+        SpeedX = SpeedX * -1;
+        SpeedY = SpeedY * -1;
     }
-
-    if(Y <= 40) {
-        SpeedX = -1;
-        SpeedY = 1;
-    }
-
-    if(Y >= 680) {
-        SpeedX = 1;
-        SpeedY = -1;
-    }
+    
 }
