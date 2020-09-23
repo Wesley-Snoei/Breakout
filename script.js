6bd2955a219fff216845dab32a5787435f772699
Wesley-Snoei <111585@emmauscollege.nl>
2020-09-23 09:31:32 +0000

    ws

--- script.js
+++ script.js
@@ -1,56 +1,45 @@
-class plank(X, Y, W, H) {
-    this.PlankX = X;
-    this.PlankY = Y;
-    this.W = W;
-    this.H = H;
-
-    show() {
-        rect(PlankX, PlankY, W, H);
-    }
-
-    update() {
-        PlankX = mouseX;
-    }
-}
-
-
-
-
-
 function setup() {
   createCanvas(1280, 720);
 
   background(0, 0, 255);
 }
 
-BalX = 640;
-BalY = 360;
-SpeedX = 50;
-SpeedY = 20;
-PlankX = mouseX;
-PlankY = 650;
-W = 75;
-H = 20;
 
+balX = 640;
+balY = 360;
+speedX = 50;
+speedY = 20;
+plusX = 0;
+plusY = 0;
 
+class PlusDing {
+    constructor(x, y) {
+        this.x = plusX;
+        this.y = plusY
+    }
+}
 
 function draw() {
 
   background(0, 0, 255);
   fill(100, 100, 255);
-  ellipse(X,Y,80,80);
+  ellipse(balX, balY, 80, 80);
 
-    X = X + SpeedX;
-    Y = Y + SpeedY;
+    balX = balX + speedX;
+    balY = balY + speedY;
 
-    if(X <= 40 || X >= 1040) {
-        SpeedX = SpeedX * -1;
-        SpeedY = SpeedY * -1;
+    if(balX <= 40 || balY >= 1040) {
+        speedX = speedX * -1;
+        speedY = speedY * -1;
     }
 
     if(Y <= 40 || Y >= 680) {
-        SpeedX = SpeedX * -1;
-        SpeedY = SpeedY * -1;
+        speedX = speedX * -1;
+        speedY = speedY * -1;
     }
+
+    for(var i = 0; i < plusjes.length; i++) [
+        show.plusjes[i];
+    ]
 
 }
